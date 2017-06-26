f194b40087673e5652fe552ed8f53d9894a1e14a
Rafael da Silva Ferreira <rafael@swift-yah.io>
2017-06-26 00:49:09 -0300

    Setup basic UITableViewController with two basic sections

--- Pokedex/Controllers/HomeController.swift
+++ Pokedex/Controllers/HomeController.swift
@@ -8,5 +8,39 @@
 
 import UIKit
 
-final class HomeController: UIViewController {
+final class HomeController: UITableViewController {
+}
+
+// MARK: UITableViewDataSource conforms
+
+extension HomeController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = indexPath.section == 0 ? "PokedexStatus" : "Pokedex"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0: return 1
+        default: return 0
+        }
+    }
+}
+
+// MARK: UITableViewDelegate conforms
+
+extension HomeController {
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
 }
