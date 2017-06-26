80068587df45652ee7407436c2ca6c2c279bae9e
Rafael da Silva Ferreira <rafael@swift-yah.io>
2017-06-26 00:27:45 -0300

    Organize all code to remove boilerplates of Xcode and create folders

---
+++ Pokedex/Commons/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  Pokedex
+//
+//  Created by Rafael Ferreira on 6/25/17.
+//  Copyright © 2017 Swift Yah. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
---
+++ Pokedex/Controllers/HomeController.swift
@@ -0,0 +1,12 @@
+//
+//  HomeController.swift
+//  Pokedex
+//
+//  Created by Rafael Ferreira on 6/25/17.
+//  Copyright © 2017 Swift Yah. All rights reserved.
+//
+
+import UIKit
+
+final class HomeController: UIViewController {
+}
